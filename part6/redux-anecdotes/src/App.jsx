95a4c48c94a69158f44f9fcc08cd07aa3bb6739e
weblearner21 <kuanypeter@gmail.com>
2025-11-27 14:37:53 -0500

    finish anecdote-redux exercises

---
+++ part6/redux-anecdotes/src/App.jsx
@@ -0,0 +1,13 @@
+import AnecdotesForm from './components/AnecdoteForm'
+import AnecdoteList from './components/AnecdoteList'
+
+const App = () => {
+  return (
+    <div>
+      <AnecdoteList />
+      <AnecdotesForm />
+    </div>
+  )
+}
+
+export default App
